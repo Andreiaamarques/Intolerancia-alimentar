730082ad0ea7386600f2e2685939bcc991419538
Andreiaamarques <andreiaamarques12@gmail.com>
2024-09-06 18:46:51 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,20 @@
+let dados = [
+    {
+    titulo : "Intolerância à lactose",
+    descricao : "A intolerância à lactose é uma condição em que o organismo não consegue digerir o açúcar presente no leite e seus derivados, a lactose. Isso ocorre devido à deficiência da enzima lactase, responsável por quebrar a lactose em moléculas menores que podem ser absorvidas pelo intestino. Os sintomas da intolerância à lactose variam de pessoa para pessoa e podem incluir inchaço abdominal, gases, diarreia e cólicas.",
+    link: "https://www.tuasaude.com/intolerancia-lactose/",
+     tags: "leite intolerancia Lactose"
+},
+    {
+    titulo : "Intolerância à frutose",
+    descricao : "A intolerância à frutose é uma condição em que o organismo não consegue digerir a frutose, um tipo de açúcar presente em frutas, mel e alguns vegetais. Isso ocorre devido à deficiência da enzima aldolase B, responsável por quebrar a frutose. Os sintomas da intolerância à frutose incluem dor abdominal, diarreia, náuseas e vômitos.",
+    link: "https://www.tuasaude.com/intolerancia-a-frutose/",
+    tags: "frutas intolerancia Frutose"
+},
+    {
+    titulo : "Intolerância ao glúten",
+    descricao : "A intolerância ao glúten, também conhecida como doença celíaca, é uma doença autoimune em que o organismo reage ao glúten, uma proteína presente no trigo, cevada e centeio. Os sintomas da doença celíaca incluem diarreia, inchaço abdominal, fadiga e perda de peso.",
+    link: "https://www.tuasaude.com/intolerancia-ao-gluten/.",
+    tags: "trigo Glúten gluten"
+}
+];
